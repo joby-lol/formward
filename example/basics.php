9ab0675246520cad52134655ad2ee830a943659f
Joby Elliott <joby@Jobys-iMac.local>
2018-08-24 10:59:58 -0600

    better example code

--- example/basics.php
+++ example/basics.php
@@ -1,4 +1,7 @@
-<?php @session_start() ?>
+<?php
+include '../vendor/autoload.php';
+@session_start();
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -9,25 +12,23 @@
     <body>
 <?php
 
-include '../vendor/autoload.php';
-
+// create a new form
 $form = new Formward\Form('Test form');
+
+// add a simple input field and mark it as required
 $form['test'] = new Formward\Fields\Input('Test field');
 $form['test']->required(true);
 
-/*
-Converting a form to a string will check its submission state and validate it if
-it is submitted. It will not call any of the callbacks. You have to call
-handle() to get those to run.
-
-This means you can echo and handle the form in any order you like. It's probably
-marginally faster to handle it first, but it's also probably not worth worrying
-about the difference.
- */
+// output the form to the page
 echo $form;
 
-echo "<h2>Form state</h2>";
+// calling $form->handle() returns one of true, false, or null, which mean:
+// true:  form submitted and validated
+// false: form submitted but has validation errors
+// null:  form not submitted
 $result = $form->handle();
+
+echo "<h2>Form state</h2>";
 if ($result === true) {
     echo "<p>Submitted and valid</p>";
 } elseif ($result === false) {
@@ -36,6 +37,7 @@ if ($result === true) {
     echo "<p>Not submitted</p>";
 }
 
+// an array of all current form values can be gotten from $form->value()
 echo "<h2>Form::value()</h2>";
 echo "<pre>";
 print_r($form->value());
