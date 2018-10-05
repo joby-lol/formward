10148ed3509e749579febedde33cf1881b1921c8
Joby Elliott <joby@Jobys-iMac.local>
2018-10-05 11:17:23 -0600

    added checkbox field

---
+++ example/classes.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file demonstrates the basic classes that come in Formward
+ */
+include '../vendor/autoload.php';
+@session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="basic-styles.css">
+    </head>
+    <body>
+<?php
+
+// create a new form
+$form = new Formward\Form('Demo form');
+
+// add a simple input field and mark it as required
+$form['checkbox'] = new Formward\Fields\Checkbox('Formward\\Fields\\Checkbox');
+
+// output the form to the page
+echo $form;
+
+// calling $form->handle() returns one of true, false, or null, which mean:
+// true:  form submitted and validated
+// false: form submitted but has validation errors
+// null:  form not submitted
+$result = $form->handle();
+
+echo "<h2>Form state</h2>";
+if ($result === true) {
+    echo "<p>Submitted and valid</p>";
+} elseif ($result === false) {
+    echo "<p>Submitted and invalid</p>";
+} elseif ($result === null) {
+    echo "<p>Not submitted</p>";
+}
+
+// an array of all current form values can be gotten from $form->value()
+echo "<h2>Form::value()</h2>";
+echo "<pre>";
+print_r($form->value());
+echo "</pre>";
+
+?>
+    </body>
+</html>
---
+++ src/Fields/Checkbox.php
@@ -0,0 +1,61 @@
+<?php
+/* Formward | https://gitlab.com/byjoby/formward | MIT License */
+namespace Formward\Fields;
+
+use Formward\FieldInterface;
+
+class Checkbox extends Input
+{
+    public function __construct(string $label, string $name=null, FieldInterface $parent=null)
+    {
+        parent::__construct($label, $name, $parent);
+        $this->type('checkbox');
+    }
+
+    /**
+     * Convert the underlying 'on' values to boolean
+     */
+    public function value($set = null)
+    {
+        if ($set !== null) {
+            parent::value($set?'on':'');
+        }
+        return parent::value() == 'on';
+    }
+
+    /**
+     * Convert the underlying 'on' values to boolean
+     */
+    public function default($set = null)
+    {
+        if ($set !== null) {
+            parent::default($set?'on':'');
+        }
+        return parent::default() == 'on';
+    }
+
+    /**
+     * Checkboxes have their label after their input tag
+     */
+    public function wrapperContentOrder() : array
+    {
+        return array(
+            '{field}',
+            '{label}',
+            '{tips}'
+        );
+    }
+
+    /**
+     * Checkboxes don't get their value put back out in HTML
+     */
+    protected function htmlAttributes()
+    {
+        $attr = parent::htmlAttributes();
+        unset($attr['value']);
+        if ($this->value()) {
+            $attr['checked'] = true;
+        }
+        return $attr;
+    }
+}
