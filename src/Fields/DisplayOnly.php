1f3e6289d5d4464f6252c898b231541c733eea27
Joby Elliott <joby@Jobys-iMac.local>
2018-10-30 12:36:50 -0600

    updated DisplayOnly

--- src/Fields/DisplayOnly.php
+++ src/Fields/DisplayOnly.php
@@ -9,13 +9,16 @@ use Formward\AbstractField;
  * content() getter/setter method to let you display HTML content of your
  * own creation, mid-form and wrapped in a FieldWrapper
  *
- * Cannot have values, and always validates successfully
+ * Cannot have values, and always validates successfully, and is also never
+ * required or disabled.
  */
 class DisplayOnly extends AbstractField
 {
     public $tag = 'div';
     public $selfClosing = false;
     protected $displayContent = '';
+    protected $required = false;
+    protected $disabled = false;
 
     public function content(string $set = null) : string
     {
@@ -23,6 +26,16 @@ class DisplayOnly extends AbstractField
             $this->displayContent = $set;
         }
         return $this->displayContent;
+    }
+
+    public function required($required = null)
+    {
+        return false;
+    }
+
+    public function disabled($disabled = null)
+    {
+        return false;
     }
 
     public function validate() : bool
