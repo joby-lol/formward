9ebe32539f64a8ede9619a10cc62210dc32ecc83
Joby Elliott <elliott.joby@gmail.com>
2020-09-04 08:44:27 -0600

    Checkbox bug fix
    The previous version of checkboxes didn't work when used in a non-standard Form container class

--- src/Fields/Checkbox.php
+++ src/Fields/Checkbox.php
@@ -3,42 +3,36 @@
 namespace Formward\Fields;
 
 use Formward\FieldInterface;
-use Formward\FormInterface;
 
 class Checkbox extends Input
 {
-    public function __construct(string $label, string $name=null, FieldInterface $parent=null)
+    protected $value = null;
+
+    public function __construct(string $label, string $name = null, FieldInterface $parent = null)
     {
         parent::__construct($label, $name, $parent);
         $this->type('checkbox');
         $this->default(false);
     }
 
-    public function submittedValue()
+    public function value($value = null)
     {
-        //locate root parent
-        if (($form = $this->rootParent()) && ($form instanceof FormInterface)) {
-            //verify that root is a Form
-            if ($form instanceof FormInterface) {
-                //check if form is submitted
-                if ($form->submitted()) {
-                    return parent::submittedValue() == 'on';
-                }
-            }
+        if ($this->value === null) {
+            return parent::value() == 'on';
+        } else {
+            return $this->value = $value;
         }
-        //if we don't have a root Form or if it isn't submitted then value is unknown
-        return null;
     }
 
     /**
      * Checkboxes have their label after their input tag
      */
-    public function wrapperContentOrder() : array
+    public function wrapperContentOrder(): array
     {
         return array(
             '{field}',
             '{label}',
-            '{tips}'
+            '{tips}',
         );
     }
 
